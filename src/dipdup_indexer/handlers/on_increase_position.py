9920ec228806c9b76012d14bf6e1433d6685f367
Rohan Pahwa <rohanphww@gmail.com>
2023-07-29 00:29:30 +0530

    FIx

--- src/dipdup_indexer/handlers/on_increase_position.py
+++ src/dipdup_indexer/handlers/on_increase_position.py
@@ -2,10 +2,13 @@ from dipdup.context import HandlerContext
 from dipdup.models import Transaction
 from dipdup_indexer.types.zenith.parameter.increase_position import IncreasePositionParameter
 from dipdup_indexer.types.zenith.storage import ZenithStorage
+from dipdup_indexer.types.vUSD.parameter.transfer import TransferParameter
+from dipdup_indexer.types.vUSD.storage import VUSDStorage
 
 
 async def on_increase_position(
     ctx: HandlerContext,
     increase_position: Transaction[IncreasePositionParameter, ZenithStorage],
+    transfer: Transaction[TransferParameter, VUSDStorage],
 ) -> None:
     ...
