d1ef372c9cfa11189fa39010b8ed9566d77978f9
OjusWiZard <ojuswimail@gmail.com>
2023-08-28 16:17:50 +0530

    🔧 chore(on_increase_position.py): refactor code to improve readability and maintainability 🐛 fix(on_increase_position.py): fix import statements to match the correct module names ✨ feat(on_increase_position.py): add logic to create or update User and IncreasePosition models based on transaction data

--- src/dipdup_indexer/handlers/on_increase_position.py
+++ src/dipdup_indexer/handlers/on_increase_position.py
@@ -1,9 +1,11 @@
 from dipdup.context import HandlerContext
 from dipdup.models import Transaction
-from dipdup_indexer.types.zenith.parameter.increase_position import IncreasePositionParameter
-from dipdup_indexer.types.zenith.storage import ZenithStorage
+
+from dipdup_indexer import models
 from dipdup_indexer.types.vUSD.parameter.transfer import TransferParameter
 from dipdup_indexer.types.vUSD.storage import VUSDStorage
+from dipdup_indexer.types.zenith.parameter.increase_position import IncreasePositionParameter
+from dipdup_indexer.types.zenith.storage import ZenithStorage
 
 
 async def on_increase_position(
@@ -11,4 +13,13 @@ async def on_increase_position(
     increase_position: Transaction[IncreasePositionParameter, ZenithStorage],
     transfer: Transaction[TransferParameter, VUSDStorage],
 ) -> None:
-    ...
+    user_address = increase_position.data.sender_address
+    user_balance = transfer.storage.balances.get(user_address, '0').balance
+    user, _ = await models.User.get_or_create(address=user_address, balance=user_balance)
+    increasePosition, _ = await models.IncreasePosition.get_or_create(
+        id=increase_position.data.id,
+        user=user,
+        amount=increase_position.parameter.vUSD_amount,
+    )
+    await user.save()
+    await increasePosition.save()
